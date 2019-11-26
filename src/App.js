870c675856e1fa59175d0311504e38362a9e46de
codedump00 <blazinasis@gmail.com>
2019-11-26 18:39:07 +0545

    fixed card fetching error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import {
-  addMerchants,
-  addMaxCount,
-  setPage,
-  addCards,
-  setCardPage,
-  addCardMaxCount
+    addMerchants,
+    addMaxCount,
+    setPage,
+    addCards,
+    setCardPage,
+    addCardMaxCount
 } from "./store/actions";
 import { fetchMerchants, getCards } from "./api/main";
 
@@ -20,116 +20,127 @@ import CustomizedSnackbars from "./components/Snackbar";
 import CardsTable from "./components/CardsTable";
 
 function MainApp({
-  token,
-  maxCount,
-  addMerchants,
-  addMaxCount,
-  merchants,
-  page,
-  setPage,
-  cardPage,
-  setCardPage,
-  addCards,
-  maxCountCards,
-  addCardMaxCount,
-  variant
-}) {
-  useEffect(() => {
-    if (maxCount === null)
-      fetchMerchants(page)
-        .then(data => {
-          if (!data.error && data.result && merchants.length === 0) {
-            addMerchants(data.result);
-            addMaxCount(data.count);
-          }
-        })
-        .catch(err => console.log(err));
-    if (maxCountCards === null && token !== null) {
-      getCards(cardPage, token)
-        .then(data => {
-          if (!data.error && data.result) {
-            addCards(data.result);
-            addCardMaxCount(data.count);
-          }
-        })
-        .catch(err => console.log(err));
-    }
-  });
-
-  useEffect(() => {
-    if (maxCount !== null) {
-      if (maxCount - page * 15 > 0) {
-        console.log("updating");
-        fetchMerchants(page + 1)
-          .then(data => {
-            if (!data.error && data.result) {
-              addMerchants(data.result);
-              setPage(page + 1);
-            }
-          })
-          .catch(err => console.log(err));
-      }
-    }
-    if (maxCountCards !== null && token !== null) {
-      if (maxCountCards - cardPage * 15 > 0) {
-        getCards(page + 1, token).then(data => {
-          if (data.result && !data.error) {
-            addCards(data.result);
-            setCardPage(page + 1);
-          }
-        });
-      }
-    }
-  }, [
-    page,
+    token,
     maxCount,
     addMerchants,
+    addMaxCount,
+    merchants,
+    cards,
+    page,
     setPage,
-    maxCountCards,
     cardPage,
-    addCards,
     setCardPage,
-    token
-  ]);
+    addCards,
+    maxCountCards,
+    addCardMaxCount,
+    variant
+}) {
+    useEffect(() => {
+        if (maxCount === null)
+            fetchMerchants(page)
+                .then(data => {
+                    if (!data.error && data.result && merchants.length === 0) {
+                        addMerchants(data.result);
+                        addMaxCount(data.count);
+                    }
+                })
+                .catch(err => console.log(err));
+        if (maxCountCards === null && token !== null) {
+            getCards(cardPage, token)
+                .then(data => {
+                    if (!data.error && data.result) {
+                        addCards(data.result);
+                        addCardMaxCount(data.count);
+                    }
+                })
+                .catch(err => console.log(err));
+        }
+    });
 
-  return (
-    <div className="App">
-      {token === null ? (
-        <InputAdornments />
-      ) : (
-        <>
-          <ElevateAppBar />
-          <FullScreenDialog />
-          <MaterialTableDemo />
-          <EditDialog />
-          <CustomizedSnackbars type={variant} />
-          <CardsTable />
-        </>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        async function getApiData() {
+            if (maxCount !== null) {
+                if (maxCount > merchants.length) {
+                    // console.log("updating", maxCount);
+                    try {
+                        const merchantsApi = await fetchMerchants(page + 1);
+                        if (!merchantsApi.error && merchantsApi.result) {
+                            addMerchants(merchantsApi.result);
+                            setPage(page + 1);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            }
+            if (maxCountCards !== null && token !== null) {
+                if (maxCountCards > cards.length) {
+                    // console.log("updating", maxCountCards, cardPage);
+                    try {
+                        const cardsApi = await getCards(cardPage + 1, token);
+                        if (cardsApi.result && cardsApi.result.length > 0) {
+                            addCards(cardsApi.result);
+                            setCardPage(cardPage + 1);
+                        }
+                    } catch (e) {
+                        console.log("error", e);
+                    }
+                }
+            }
+        }
+        getApiData();
+    }, [
+        page,
+        maxCount,
+        addMerchants,
+        setPage,
+        maxCountCards,
+        cardPage,
+        addCards,
+        setCardPage,
+        token,
+        merchants,
+        cards
+    ]);
+
+    return (
+        <div className="App">
+            {token === null ? (
+                <InputAdornments />
+            ) : (
+                <>
+                    <ElevateAppBar />
+                    <FullScreenDialog />
+                    <MaterialTableDemo />
+                    <EditDialog />
+                    <CustomizedSnackbars type={variant} />
+                    <CardsTable />
+                </>
+            )}
+        </div>
+    );
 }
 
 const mapDispatchToProps = {
-  addMerchants,
-  setPage,
-  addCards,
-  setCardPage,
-  addCardMaxCount,
-  addMaxCount
+    addMerchants,
+    setPage,
+    addCards,
+    setCardPage,
+    addCardMaxCount,
+    addMaxCount
 };
 
 const mapStateToProps = state => {
-  return {
-    token: state.merchant.token,
-    merchants: state.merchant.merchants,
-    maxCount: state.merchant.maxCount,
-    page: state.merchant.page,
-    cardPage: state.card.page,
-    cards: state.card.cards,
-    maxCountCards: state.card.maxCount,
-    variant: state.ui.variant
-  };
+    return {
+        token: state.merchant.token,
+        merchants: state.merchant.merchants,
+        maxCount: state.merchant.maxCount,
+        page: state.merchant.page,
+        cardPage: state.card.page,
+        cards: state.card.cards,
+        maxCountCards: state.card.maxCount,
+        variant: state.ui.variant
+    };
 };
 
 const App = connect(mapStateToProps, mapDispatchToProps)(MainApp);
